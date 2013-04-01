0775381f8f33a4fea113b6febb4bb2821332ef80
Alessandro Di Maria <adm@m42.ch>
2013-04-02 01:23:22 +0200

    level configurations helpers

--- app/helpers/admin/levels_helper.rb
+++ app/helpers/admin/levels_helper.rb
@@ -1,2 +1,7 @@
 module Admin::LevelsHelper
+
+  def level_configuration_files
+    config_files = Rails.root.join('config', 'levels').children.map {|config_file| config_file.split[1].to_s}
+  end
+
 end
