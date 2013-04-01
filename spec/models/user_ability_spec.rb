2e7476ac33866c5ffb0cdaa9b5ba44b627846c03
Alessandro Di Maria <adm@m42.ch>
2013-04-01 23:16:20 +0200

    refactor user ability spec

--- spec/models/user_ability_spec.rb
+++ spec/models/user_ability_spec.rb
@@ -2,52 +2,32 @@ require 'spec_helper'
 
 describe Ability do
 
-  describe 'role admin' do
-    before :each do
-      @admin = FactoryGirl.create :admin_user
-    end
+  let(:user) { FactoryGirl.create :user }
 
-    it "should be able to manage users" do
-      user = FactoryGirl.create :user
-      @admin.should have_ability_to :read, user
-      @admin.should have_ability_to :create, User
-      @admin.should have_ability_to :update, user
-      @admin.should have_ability_to :destroy, user
-    end
+  describe 'role admin' do
+    subject(:admin) { FactoryGirl.create :admin_user }
+    it { should have_ability_to :read, user }
+    it { should have_ability_to :create, User }
+    it { should have_ability_to :update, user }
+    it { should have_ability_to :destroy, user }
   end
 
   describe 'role member' do
-    before :each do
-      @member = FactoryGirl.create :user
-    end
-
-    it "should only be able to update his own user" do
-      user = FactoryGirl.create :user
-      @member.should have_ability_to :read, user
-      @member.should have_ability_to :update, @member
-
-      @member.should_not have_ability_to :create, User
-      @member.should_not have_ability_to :update, user
-      @member.should_not have_ability_to :destroy, user
-      @member.should_not have_ability_to :destroy, @member
-    end
+    subject(:member) { FactoryGirl.create :user }
+    it { should have_ability_to :read, user }
+    it { should have_ability_to :update, member }
+    it { should_not have_ability_to :create, User }
+    it { should_not have_ability_to :update, user }
+    it { should_not have_ability_to :destroy, user }
+    it { should_not have_ability_to :destroy, member }
   end
 
   describe 'role guest' do
-
-    before :each do
-      @guest = User.new
-    end
-
-    it "should be able to register" do
-      user = FactoryGirl.create :user
-      @guest.should have_ability_to :read, User
-      @guest.should have_ability_to :create, User
-
-      @guest.should_not have_ability_to :update, user
-      @guest.should_not have_ability_to :destroy, user
-    end
-
+    subject(:guest) { User.new }
+    it { should have_ability_to :read, User }
+    it { should have_ability_to :create, User }
+    it { should_not have_ability_to :update, user }
+    it { should_not have_ability_to :destroy, user }
   end
 
 end
