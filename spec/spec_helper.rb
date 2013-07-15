ce283311fa5df10f79bb8c71f2e5cfdf8a1e919e
Alessandro Di Maria <adm@m42.ch>
2013-07-15 22:20:06 +0200

    disable should syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,9 @@ Spork.prefork do
 
   RSpec.configure do |config|
 
+    config.expect_with :rspec do |c|
+      c.syntax = :expect
+    end
     config.fail_fast = true
 
     config.filter_run :focus => true
