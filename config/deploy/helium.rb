fa432ef9ed7dc4cc0e1042a4b8810f25b442f2c9
Alessandro Di Maria <adm@m42.ch>
2013-09-11 16:58:02 +0200

    deployment for helium

---
+++ config/deploy/helium.rb
@@ -0,0 +1,9 @@
+server "127.0.0.1", :app, :web, :db, :primary => true
+set :port, 2222
+set :branch,  "puma"
+set :rails_env, "production"
+
+set :deploy_to,   "/srv/sites/dawning.zero-x.net"
+
+set :rvm_ruby_string, '2.0.0'
+set :puma_config_file, "puma.rb"
