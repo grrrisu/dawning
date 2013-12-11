3ee09d0b16d6b226c928b062828717feffacfc8f
Alessandro Di Maria <adm@m42.ch>
2013-12-11 21:41:13 +0100

    fix Rocky begining with oe

--- db/sample_seeds.rb
+++ db/sample_seeds.rb
@@ -1,6 +1,6 @@
 unless User.find_by_username('Rocky 1')
   20.times do |n|
-    user = User.new(username: "Rocky #{n}", password: 'rocky', password_confirmation: 'rocky', email: "rocky#{n}@example.com")
+    user = User.new(username: "Rocky #{n+1}", password: 'rocky', password_confirmation: 'rocky', email: "rocky#{n+1}@example.com")
     user.save!
     user.activate!
   end
