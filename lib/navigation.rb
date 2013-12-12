bbf37b143d657c7590e265f9787af75f7e5d0eab
Alessandro Di Maria <adm@m42.ch>
2013-12-12 23:24:14 +0100

    set page title

--- lib/navigation.rb
+++ lib/navigation.rb
@@ -1,15 +1,15 @@
 module Navigation
-  
+
   def self.included(base)
     base.extend ClassMethods
     base.instance_eval do
-      helper_method :navigation_active?
+      helper_method :navigation_active?, :page_title
     end
     base.send(:include, InstanceMethods)
   end
-  
+
   module ClassMethods
-    
+
     def navigation *breadcrumbs
       @breadcrumbs = breadcrumbs
     end
@@ -17,19 +17,23 @@ module Navigation
     def breadcrumbs
       @breadcrumbs || []
     end
-    
+
   end
-  
+
   module InstanceMethods
-    
+
+    def page_title
+      self.class.breadcrumbs.map(&:to_s).map(&:titleize).join(' ')
+    end
+
     def navigation *breadcrumbs
       self.class.navigation *breadcrumbs
     end
-    
+
     def navigation_active? key
       self.class.breadcrumbs.include?(key)
     end
-    
+
   end
-  
+
 end
