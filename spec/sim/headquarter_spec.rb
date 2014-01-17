70ab8e9b19ff2b99e815384f38c8c5ef8176cf3b
Alessandro Di Maria <adm@m42.ch>
2014-01-17 21:50:08 +0100

    add headquarter and view spec

---
+++ spec/sim/headquarter_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Headquarter do
+
+  before(:each) do
+    @headquarter = Headquarter.new(5,5)
+  end
+
+  describe "create view" do
+
+    before(:each) do
+      @headquarter.pawns << Pawn.new(4,5)
+      config  = {world: {height: 10, width: 10}}
+      world   = World.new(11).build(config)
+      @view   = View.new(world, 0, 0, world.width, world.height)
+      @view.unfog(@headquarter)
+      @headquarter.pawns.each {|pawn| @view.unfog(pawn) }
+    end
+
+    it "with size of 11" do
+      expect(@view.size).to be == [11,11]
+    end
+
+    it "at position 2,2" do
+      expect(@view.x).to be == 0
+      expect(@view.y).to be == 0
+    end
+
+    it "has set visability" do
+      expect(@view[2,2]).to be == 0  # outside of any view
+      expect(@view[5,5]).to be == 2  # hq & pawn
+      expect(@view[6,6]).to be == 1  # only hq
+    end
+
+  end
+
+end
---
+++ spec/sim/view_spec.rb
@@ -0,0 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+module Sim
+  class Matrix
+
+
+
+  end
+end
+
+describe View do
+
+  before(:each) do
+    world = World.new(8, 8)
+    world.set_each_field { 'w' }
+    @view = View.new(world, 0, 0, 5)
+  end
+
+  it 'should detect wihtin radius' do
+    expect(View.within_radius(1,1,2)).to be_true
+    expect(View.within_radius(2,2,2)).to be_false
+    expect(View.within_radius(1,2,2)).to be_true
+    expect(View.within_radius(1,2,2,0)).to be_false
+  end
+
+  describe 'with pawn' do
+
+    before(:each) do
+      pawn = Pawn.new(1,1)
+      @view.unfog(pawn)
+    end
+
+    it "should set pawns visibility" do
+      expected = [[1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+      expect(@view.fields).to be == expected
+    end
+
+    it "should filter pawns visibility to world" do
+      expected = [['w', 'w', 'w', nil, nil], ['w', 'w', 'w', nil, nil], ['w', 'w', 'w', nil, nil], [nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil]]
+      expect(@view.filter.fields).to be == expected
+    end
+
+  end
+
+end
