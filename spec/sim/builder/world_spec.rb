4a41c657e402229ad70460b79238ba14c6b08a69
Alessandro Di Maria <adm@m42.ch>
2014-02-26 22:24:15 +0100

    move check boundaries spec to world spec

--- spec/sim/builder/world_spec.rb
+++ spec/sim/builder/world_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Builder::World do
 
-  let(:world)   { World.new(15, 30) }
+  let(:world)   { World.new(15, 15) }
   let(:config)  { Sim::Buildable.load_config(File.join(Rails.root, 'config', 'levels', 'test.yml'))[:world][:builder] }
   let(:builder) { Builder::World.new(world) }
 
@@ -11,26 +11,6 @@ describe Builder::World do
     world.each_field do |field|
       expect(field.vegetation?).to be_true
     end
-  end
-
-  describe "world bounderies" do
-
-    it "should check bounderies" do
-      expect(world.check_bounderies(0, 0)).to be == [0,0]
-      expect(world.check_bounderies(15, 30)).to be == [0, 0]
-      expect(world.check_bounderies(5, 10)).to be == [5, 10]
-      expect(world.check_bounderies(-5, -10)).to be == [14, 29]
-      expect(world.check_bounderies(25, 50)).to be == [0, 0]
-    end
-
-    it "should check bounderies", skip: true do
-      expect(world.position_bounderies(0, 0)).to be == [0,0]
-      expect(world.position_bounderies(15, 30)).to be == [0, 0]
-      expect(world.position_bounderies(5, 10)).to be == [5, 10]
-      expect(world.position_bounderies(-5, -10)).to be == [10, 20]
-      expect(world.position_bounderies(25, 50)).to be == [10, 20]
-    end
-
   end
 
 end
--- spec/sim/world_spec.rb
+++ spec/sim/world_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe World do
 
-  subject(:level) { Level.new }
+  let(:world)   { World.new(15, 30) }
 
-  it "should create the world" do
+  describe "world bounderies" do
+
+    it "should check bounderies" do
+      expect(world.check_bounderies(0, 0)).to be == [0,0]
+      expect(world.check_bounderies(15, 30)).to be == [0, 0]
+      expect(world.check_bounderies(5, 10)).to be == [5, 10]
+      expect(world.check_bounderies(-5, -10)).to be == [14, 29]
+      expect(world.check_bounderies(25, 50)).to be == [0, 0]
+    end
+
+    it "should check bounderies", skip: true do
+      expect(world.position_bounderies(0, 0)).to be == [0,0]
+      expect(world.position_bounderies(15, 30)).to be == [0, 0]
+      expect(world.position_bounderies(5, 10)).to be == [5, 10]
+      expect(world.position_bounderies(-5, -10)).to be == [10, 20]
+      expect(world.position_bounderies(25, 50)).to be == [10, 20]
+    end
+
   end
 
 end
