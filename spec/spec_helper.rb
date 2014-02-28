f5364d34e7034dd08a27941cd1236a77a0b5b3cc
Alessandro Di Maria <adm@m42.ch>
2014-02-28 22:25:51 +0100

    require features support files first in prefork block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,7 +52,8 @@ Spork.prefork do
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("spec/support/features/*.rb")].each {|f| require f} # require features first
+  Dir[Rails.root.join("spec/support/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
 
