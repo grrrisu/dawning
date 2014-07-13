ea73333f6d6ede8331061075d1f54da37b390ab3
Alessandro Di Maria <adm@m42.ch>
2014-07-13 15:16:30 +0200

    fix player spec

--- spec/sim/player_spec.rb
+++ spec/sim/player_spec.rb
@@ -5,7 +5,7 @@ describe Player do
   let(:world)       { World.new(3,3) }
   let(:view)        { View.new(world, 0, 0, world.height)}
   let(:headquarter) { Headquarter.new(1,1) }
-  let(:player)      { Player.new(view, headquarter) }
+  let(:player)      { Player.new('123', double(:level)) }
 
   describe 'move' do
 
@@ -13,14 +13,15 @@ describe Player do
       world[1,1].pawn = headquarter
       headquarter.pawns << @pawn = Pawn.new(0, 1)
       world[0,1].pawn = @pawn
+      player.place(view, headquarter)
     end
 
     it "should move pawn to an empty field" do
-      expect(player.move(@pawn.id, 0, 0)).to be == {x: 0, y: 0}
+      expect(player.move(@pawn.id, 0, 0)).to be == {pawn_id: @pawn.id, x: 0, y: 0}
     end
 
     it "should not move a pawn to an occupied field" do
-      expect(player.move(@pawn.id, 1, 1)).to be == {x: 0, y: 1}
+      expect(player.move(@pawn.id, 1, 1)).to be == {pawn_id: @pawn.id, x: 0, y: 1}
     end
 
   end
