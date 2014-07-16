37cd588818e3b69c4f419fc044a64914ad2fbf72
Alessandro Di Maria <adm@m42.ch>
2014-07-16 21:38:24 +0200

    don’t use port in production and staging environment for websocket url

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -4,4 +4,12 @@ module MapsHelper
     @images.inject({}) {|h, image| h[image[0]] = asset_path(image[1]); h }
   end
 
+  def websocket_url
+    if Rails.env.production? || Rails.env.staging?
+      "#{request.host}/websocket"
+    else
+      "#{request.host}:#{request.port}/websocket"
+    end
+  end
+
 end
