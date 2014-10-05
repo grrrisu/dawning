0f8568a3e83a9cf5750de39b08b04c9b12e66ed5
Alessandro Di Maria <adm@m42.ch>
2014-10-05 14:40:14 +0200

    + add specs for player base overlap_current_view?

--- lib/sim/player/base.rb
+++ lib/sim/player/base.rb
@@ -6,7 +6,22 @@ module Player
     end
 
     def view x, y, width, height
-      self.current_view_dimension = {x: x, y: y, width: width, height: height}
+      self.current_view_dimension = Hashie::Mash.new x: x, y: y, width: width, height: height
+    end
+
+    def overlap_current_view? other
+      view = current_view_dimension
+      horizontal_range = (view.x)..(view.x + view.width)
+      vertical_range   = (view.y)..(view.y + view.height)
+      return true if horizontal_range.include?(other.x) &&
+                     vertical_range.include?(other.y)
+      return true if horizontal_range.include?(other.x + other.width) &&
+                     vertical_range.include?(other.y)
+      return true if horizontal_range.include?(other.x) &&
+                     vertical_range.include?(other.y + other.height)
+      return true if horizontal_range.include?(other.x + other.width) &&
+                     vertical_range.include?(other.y + other.height)
+      false
     end
 
   end
---
+++ spec/sim/player/base_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Player::Base do
+
+  let(:player)      { Player::Base.new('id', 'level') }
+  let(:view_params) { {x: 2, y: 5, width: 4, height: 5} }
+
+  before(:each) { player.view(*view_params.values) }
+
+  it "should overlap if self" do
+    expect(player.overlap_current_view? player.current_view_dimension).to eq(true)
+  end
+
+  it "should overlap if left top is included" do
+    other = Hashie::Mash.new x: 3, y: 8, width: 4, height: 5
+    expect(player.overlap_current_view? other).to eq(true)
+  end
+
+  it "should overlap if right top is included" do
+    other = Hashie::Mash.new x: 1, y: 8, width: 4, height: 5
+    expect(player.overlap_current_view? other).to eq(true)
+  end
+
+  it "should overlap if left bottom is included" do
+    other = Hashie::Mash.new x: 1, y: 2, width: 4, height: 5
+    expect(player.overlap_current_view? other).to eq(true)
+  end
+
+  it "should overlap if right bottom is included" do
+    other = Hashie::Mash.new x: 3, y: 2, width: 4, height: 5
+    expect(player.overlap_current_view? other).to eq(true)
+  end
+
+  it "should not overlap if left top is outside" do
+    other = Hashie::Mash.new x: 7, y: 8, width: 4, height: 5
+    expect(player.overlap_current_view? other).to eq(false)
+  end
+
+end
