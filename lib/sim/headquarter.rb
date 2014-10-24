d18500e8f3a27a72ac5ac3368bb303465e4e60e5
Alessandro Di Maria <adm@m42.ch>
2014-10-24 21:55:55 +0200

    + increase move radius of pawns

--- lib/sim/headquarter.rb
+++ lib/sim/headquarter.rb
@@ -19,7 +19,7 @@ class Headquarter < Pawn
 
   def within_influence_area x, y
     dx, dy = self.x - x, self.y - y
-    if View.within_radius(dx, dy, view_radius)
+    if View.within_radius(dx, dy, view_radius * 0.5)
       yield
     else
       $stderr.puts "movement[#{dx},#{dy}] not within influence area[#{view_radius}, #{x}, #{y}] "
