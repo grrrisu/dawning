2261907cb1692d1f3734bd3a47ce7a5a81a1f632
Alessandro Di Maria <adm@m42.ch>
2014-10-26 20:14:36 +0100

    ! reproduce while aging, independent of health

--- lib/sim/animal.rb
+++ lib/sim/animal.rb
@@ -1,34 +1,44 @@
-class Animal
+class Animal < Sim::Object
 
   default_attr :age, 0
-  default_attr :age_treshold, 30
-  default_attr :hunger, 50
-  default_attr :reproduce_treshold, 100
+  default_attr :age_death, 30
+  default_attr :health, 50
+  defautl_attr :max_health, 100
+  default_attr :birth_step, 5
+  default_attr :next_birth, 0
   default_attr :needed_food, 20
+  default_attr :max_food, 30
 
   attr_accessor :field
 
+  def sim
+    area = super
+    aging delay
+    think
+    area
+  end
+
   def calculate step
-    aging(step)
     eat(step)
   end
 
-  def aging step
-    self.age += step
-    die! if age >= age_treshold
+  def eat step
+    self.health += (food_eaten - needed_food) * step
+    self.health = max_health if health > max_health
+    die! if health <= 0
   end
 
-  def eat step
-    self.hunger += food_eaten - needed_food
-    if hunger <= 0
-      die!
-    elsif hunger >= reproduce_treshold
-      reproduce
-    end
+  def aging step
+    self.next_birth += step
+    self.age += step
+    die! if age >= age_death
+    reproduce if next_birth >= birth_step
   end
 
   def food_eaten
-
+    field.vegetation.sim
+    food_available = field.vegetation.size
+    # TODO smart formula
   end
 
   def think
@@ -41,6 +51,8 @@ class Animal
   end
 
   def reproduce
+    self.next_birth -= birth_step
+    # TODO fire reproduce event
   end
 
   def create_event
