c5aadafc1c10e1af80e358ec9306ba0fba1754b5
Alessandro Di Maria <adm@m42.ch>
2014-11-01 11:53:50 +0100

    - remove old line

--- app/helpers/admin/levels_helper.rb
+++ app/helpers/admin/levels_helper.rb
@@ -3,7 +3,6 @@ module Admin::LevelsHelper
   def level_configuration_files
     config_files = Dir.glob(Rails.root.join('config', 'levels', '*.yml'))
     config_files.map {|config_file| config_file.split('/').last }
-    #config_files = Rails.root.join('config', 'levels').children.map {|config_file| config_file.split[1].to_s}
   end
 
 end
