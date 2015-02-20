a026a3e83c1dcd87e660265d31fc2fec43e07005
Alessandro Di Maria <adm@m42.ch>
2015-02-20 10:48:04 +0100

    ! link thin config, restart after publishing

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,25 +3,17 @@ set :repo_url, 'git@github.com:grrrisu/dawning.git'
 
 set :rvm_ruby_version, File.read(".ruby-version").strip
 
-# Default value for :log_level is :debug
 set :log_level, :info
 
-set :linked_files, %w{config/mongoid.yml config/secrets.yml config/level.yml}
+set :linked_files, %w{config/mongoid.yml config/secrets.yml config/level.yml config/thin/production.yml}
 set :linked_dirs, %w{log tmp/pids tmp/sockets public/system public/assets}
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 set :ssh_options, {
   user: "grrrisu",
   forward_agent: true
 }
+
+after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
 
