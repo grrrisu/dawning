fd943b7090cac7870b5ada623879934a3121fbc6
Alessandro Di Maria <adm@m42.ch>
2016-07-22 13:58:25 +0200

    adjust helium deploy to work on helium

--- config/deploy/helium.rb
+++ config/deploy/helium.rb
@@ -2,7 +2,8 @@ role :web, '127.0.0.1:2222'
 role :app, '127.0.0.1:2222'
 role :db,  '127.0.0.1:2222', primary: true
 
-set :branch,    "master"
+set :branch,    "argon"
 set :deploy_to, "/srv/sites/dawning.zero-x.net"
 
 set :rails_env, "production"
+set :thin_config_path, "config/thin/production.yml"
