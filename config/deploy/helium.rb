e8915354631f5ca41e2cfe4975c018cb226b6534
Alessandro Di Maria <adm@m42.ch>
2016-07-23 22:04:34 +0200

    set branch for helium to master

--- config/deploy/helium.rb
+++ config/deploy/helium.rb
@@ -2,7 +2,7 @@ role :web, '127.0.0.1:2222'
 role :app, '127.0.0.1:2222'
 role :db,  '127.0.0.1:2222', primary: true
 
-set :branch,    "argon"
+set :branch,    "master"
 set :deploy_to, "/srv/sites/dawning.zero-x.net"
 
 set :rails_env, "production"
