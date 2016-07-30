cfd41a545e4203e699e368672103582abda53a3c
Alessandro Di Maria <adm@m42.ch>
2016-07-30 13:09:42 +0200

    deploy production to argon

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :web, 'neon'
-role :app, 'neon'
-role :db,  'neon', primary: true
+role :web, 'argon'
+role :app, 'argon'
+role :db,  'argon', primary: true
 
 set :branch,    "production"
 set :deploy_to, "/srv/sites/dawning.zero-x.net"
